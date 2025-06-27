78c90b1b2514ed55b4df272453ededc7d1dab162
EvDes190 <89368408+EvDes190@users.noreply.github.com>
2025-06-27 04:59:23 +0300

    .

--- src/tptas/TotalPlaytimeOfAllSaves.java
+++ src/tptas/TotalPlaytimeOfAllSaves.java
@@ -5,25 +5,32 @@ import arc.util.*;
 import mindustry.mod.*;
 import mindustry.game.Saves.*;
 
+import static arc.util.Strings.formatMillis;
 import static mindustry.Vars.control;
 
 public class TotalPlaytimeOfAllSaves extends Mod{
-    Seq<SaveSlot> saves1 = control.saves.getSaveSlots();
-
-    public TotalPlaytimeOfAllSaves(){
-        getTime();
-    }
+    Seq<SaveSlot> saves1;
 
     public void getTime() {
+        saves1 = control.saves.getSaveSlots();
         long time = 0;
-        control.saves.load();
+        long sectorTime = 0;
         for (SaveSlot save : saves1) {
             time += save.meta.timePlayed;
-            Log.info(save.getPlayTime() + " " + save.meta.timePlayed + " ms");
+            sectorTime += save.isSector() ? save.meta.timePlayed : 0;
+            Log.info("[TPTAS] "+ save.getPlayTime() + " " + save.meta.timePlayed + " ms");
         }
-        float time1 = (float) time / (1000 * 60 * 60);
-        Log.info("[TPTAS] Total playtime of all saves: " + time1 + " hours");
+        String time1 = formatMillis(time);
+        String sectorTime1 = formatMillis(sectorTime);
+        Log.info("[TPTAS] Total playtime of all saves: " + time1);
+        Log.info("[TPTAS] Total playtime on campaign: " + sectorTime1);
+
     }
 
+    @Override
+    public void init() {
+        super.init();
+        getTime();
+    }
 }
 
